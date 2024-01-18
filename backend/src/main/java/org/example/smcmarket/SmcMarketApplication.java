c370c34a601f3c884527fe79b5968d1602957dc8
Jinseong <hdss8234@gmail.com>
2024-01-18 12:27:19 +0900

    백엔드 초기세팅

---
+++ backend/src/main/java/org/example/smcmarket/SmcMarketApplication.java
@@ -0,0 +1,14 @@
+package org.example.smcmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmcMarketApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(SmcMarketApplication.class, args);
+    }
+
+}
